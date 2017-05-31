85a3f472c55d9f21f2ed9a5ec129767c138c0eb9
Leon <1561888111@qq.com>
2017-05-31 19:47:03 +0800

    0.0.6
    0.0.6

---
+++ HN.UI/Controllers/AgentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HN.UI.Controllers
+{
+    public class AgentController : Controller
+    {
+        /// <summary>
+        /// 创建代理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CreateLevel() {
+            return View();
+        }
+
+        /// <summary>
+        /// 代理详情页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ HN.UI/Controllers/ProjectController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HN.UI.Controllers
+{
+    public class ProjectController : Controller
+    {
+        /// <summary>
+        /// 创建项目
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create() {
+            return View();
+        }
+
+    }
+}
